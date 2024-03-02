19341cd6d172fdfd3a59eab0c73a71bcff548872
2018wzh <wzh20070720@outlook.com>
2024-03-02 19:59:29 +0800

    refactor

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ARCH=amd64
+VERSION=$(wget -qO- -t1 -T2 "https://api.github.com/repos/fatedier/frp/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g')
+FILENAME="frp_"${VERSION}"_linux_"${ARCH}
+URL="https://github.com/fatedier/frp/releases/download/v"${VERSION}"/"${FILENAME}".tar.gz"
+
+rm -rfv ./build/*
+rm -rfv ./frp/etc/frp/*
+rm -rfv ./frp/usr/bin/*
+rm -rfv ./DEBIAN/control
+
+sed 's/%ARCH%/'${ARCH}'/g;s/%VERSION%/'${VERSION}'/g' DEBIAN/control.template | tee ./DEBIAN/control
+
+wget ${URL} -O ./build/frp.tar.gz
+
+cd ./build
+tar xvf frp.tar.gz
+mv ${FILENAME}/*.toml ../frp/etc/frp/
+chmod +x ${FILENAME}/frp*
+mv ${FILENAME}/frp* ../frp/usr/bin/
+
+dpkg-deb --build frp out/$FILENAME.deb
